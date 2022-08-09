7d391a592920c57dbfdc76dad3468ed77a05817b
Benoit Daloze <benoit.daloze@oracle.com>
2022-08-09 17:14:03 +0200

    [TMP] Commit example

---
+++ pattern_matching.rb
@@ -0,0 +1,20 @@
+v = [0, [1, 2, 3], 42]
+case v
+in [e, d, 42] # pattern e=0
+  p [e, d]
+else
+  p :failed
+end
+
+__END__
+(RootParseNode,
+  (BlockParseNode,
+    (LocalAsgnParseNode:v,
+      (ArrayParseNode, (FixnumParseNode[value=0]), (ArrayParseNode, (FixnumParseNode[value=1]), (FixnumParseNode[value=2]), (FixnumParseNode[value=3])))
+    ),
+    (CaseInParseNode, # case
+      (LocalVarParseNode:v), # expression
+      (ArrayParseNode, # list of in's
+        (InParseNode, # in
+          (ArrayPatternParseNode, (ArrayParseNode, (LocalAsgnParseNode:e, ), (LocalAsgnParseNode:d, ))), # pattern
+          (FCallParseNode:p, (ArrayParseNode, (ArrayParseNode, (LocalVarParseNode:e), (LocalVarParseNode:d))), null), null))))) # body
