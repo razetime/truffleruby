f43f2b0b361c5fb4c72fda10902fcd1c72bc050b
razetime <raghuallthetime@hotmail.com>
2022-08-23 22:56:28 +0530

    fix problem with tLBRACK, tRBRACK, tLBRACE, tRBRACE in parser

--- pattern_matching.rb
+++ pattern_matching.rb
@@ -1,20 +1,6 @@
-v = [-1, 0, [1, 2, 3], 4, 5, 42]
-case v
-in [y, e, *a, c, d] # pattern e=0
-  p [y, e, c, d]
+case 1
+in 1 | 0
+  :matched
 else
-  p :failed
+  "not matched"
 end
-
-__END__
-(RootParseNode,
-  (BlockParseNode,
-    (LocalAsgnParseNode:v,
-      (ArrayParseNode, (FixnumParseNode[value=0]), (ArrayParseNode, (FixnumParseNode[value=1]), (FixnumParseNode[value=2]), (FixnumParseNode[value=3])))
-    ),
-    (CaseInParseNode, # case
-      (LocalVarParseNode:v), # expression
-      (ArrayParseNode, # list of in's
-        (InParseNode, # in
-          (ArrayPatternParseNode, (ArrayParseNode, (LocalAsgnParseNode:e, ), (LocalAsgnParseNode:d, ))), # pattern
-          (FCallParseNode:p, (ArrayParseNode, (ArrayParseNode, (LocalVarParseNode:e), (LocalVarParseNode:d))), null), null))))) # body
