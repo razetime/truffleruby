fcbf030ea4816f40c94f9689df905814ebc74ab5
razetime <raghuallthetime@hotmail.com>
2022-09-01 19:33:10 +0530

    deconstruct changes pt1

--- pattern_matching.rb
+++ pattern_matching.rb
@@ -1,7 +1,16 @@
-a=3
-case 0
-in ^a
+# a=3
+# case [0]
+# in ^a
+#   p a
+# else
+#   "not matched"
+# end
+
+Point = Struct.new(:x, :y)
+p [1,2].deconstruct
+p (Point[1,2]).deconstruct
+case Point[1,2]
+in [a,b]
   p a
-else
-  "not matched"
+  p b
 end
